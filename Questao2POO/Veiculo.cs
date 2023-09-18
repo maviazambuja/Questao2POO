924bb416e042e5456b78a7ba94867cc366db4153
Maria Vitória Myron <mavimyron.academia@gmail.com>
2023-09-18 17:57:36 -0300

    classe mãe: Veículo

---
+++ Questao2POO/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2POO
+{
+    public abstract class Veiculo
+    {//classe mãe
+
+        private string placa;
+        private int ano;
+
+
+        protected Veiculo(string placa , int ano) 
+        {
+            this.placa = placa;
+            this.ano = ano;
+        
+        }
+        protected string Placa { get => placa; set => placa = value; }
+        protected int Ano { get => ano; set => ano = value; }
+    }
+}
