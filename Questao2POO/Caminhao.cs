139ec06aff9b252839375401be64b5c57e3362e5
Maria Vitória Myron <mavimyron.academia@gmail.com>
2023-09-18 18:14:23 -0300

    classe filha + conta

---
+++ Questao2POO/Caminhao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2POO
+{//classe filha
+    class Caminhao : Veiculo
+    {
+
+        private int eixos;
+
+        public Caminhao(string Placa, int Ano, int eixos) : base(Placa, Ano) 
+        { 
+            this.eixos = eixos;
+        
+        }
+
+        public int Eixos { get => eixos; set => eixos = value; }
+
+        public override double CalculoAluguel()
+        {
+            double diaria;
+            diaria = (300 * eixos) - (2023 - Ano) * 50;
+            return diaria;
+        }
+    }
+}
---
+++ Questao2POO/Onibus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2POO
+{//classe filha
+    class Onibus :   Veiculo
+    {
+
+        private int Assentos;
+
+        public Onibus(string Placa, int Ano, int Assentos) : base(Placa, Ano)
+        {
+            this.Assentos = Assentos;
+
+        }
+
+        public int Assentos1 { get => Assentos; set => Assentos = value; }
+
+        public override double CalculoAluguel()
+        {
+            double diaria;
+            diaria = (30 * Assentos)-(2023 - Ano) * 70;
+            return diaria;
+        }
+
+      
+    }
+}
